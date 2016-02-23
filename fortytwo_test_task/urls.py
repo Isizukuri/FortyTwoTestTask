658402bd04bfc82f99f1b6e341c2cb7f902f3879
Isizukuri <ragnarok49@gmail.com>
2016-02-23 19:03:38 +0200

    [+] added contacts app, modified base.html and added hompage template

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.views.generic import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns(
-    '',
-    # Examples:
-    # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    #contacts urls
+    # admin urls
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='contacts/index.html'), name='home'),
 )
