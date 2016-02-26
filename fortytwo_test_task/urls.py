4da34586f3fc06275980265a2797737fca6b3fb5
Isizukuri <ragnarok49@gmail.com>
2016-02-26 10:14:10 +0200

    ticket:1 fifth try to fix static

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 
 from apps.contacts import views as contact_views
@@ -13,6 +13,4 @@ urlpatterns = patterns(
     # contacts urls
     url(r'^$', contact_views.HomePageView.as_view(), name='home'),
 )
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
