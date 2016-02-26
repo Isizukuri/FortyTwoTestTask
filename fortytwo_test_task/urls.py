1311d0c21808d25ee6e9c58b2b7c21f4091773f4
Isizukuri <ragnarok49@gmail.com>
2016-02-26 10:58:56 +0200

    ticket:1 six try to fix static

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf import settings
 
 from apps.contacts import views as contact_views
 admin.autodiscover()
