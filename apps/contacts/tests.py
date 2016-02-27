8660328e38e25a5d640052aa601e6dbb8a728662
Isizukuri <ragnarok49@gmail.com>
2016-02-27 14:28:00 +0200

    ticket:1 added bonus tests, fixed Makefile flake8 config

--- apps/contacts/tests.py
+++ apps/contacts/tests.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
 
@@ -36,7 +37,7 @@ class TestHomePage(TestCase):
         self.assertTemplateUsed(response, 'contacts/index.html')
 
     def test_assert_other_data_at_page(self):
-        """Check present all contacts data on the home page"""
+        """Check present help text on the home page"""
         response = self.client.get(self.url)
         self.assertIn('Name:', response.content)
         self.assertIn('Last name:', response.content)
@@ -59,3 +60,35 @@ class TestHomePage(TestCase):
         Contact.objects.all().delete()
         response = self.client.get(self.url)
         self.assertContains(response, 'No contact data.')
+
+    def test_cyr_input(self):
+        """"Test cyrillic input"""
+        Contact.objects.all().delete()
+        contact_data = dict(
+            first_name='Люк',
+            last_name='Скайвокер',
+            bio='Блудний син Дарта Вейдера.',
+            other_contacts='Писати на село до діда Йоди.',
+        )
+        contact = Contact(**contact_data)
+        contact.save()
+        response = self.client.get(self.url)
+        self.assertIn('Люк', response.content)
+        self.assertIn('Скайвокер', response.content)
+        self.assertIn('Блудний син Дарта Вейдера.', response.content)
+        self.assertIn('Писати на село до діда Йоди', response.content)
+
+    def test_two_or_more_db_entries(self):
+        """Test in case of two or more contacts in db"""
+        contact_data = dict(
+            first_name='Qui-Gonn',
+            last_name='Jinn',
+            email='qui-gonn.jinn@jedi.com'
+        )
+        contact = Contact(**contact_data)
+        contact.save()
+        self.assertEqual(Contact.objects.all().count(), 3)
+        response = self.client.get(self.url)
+        test_object = response.context['object']
+        self.assertEqual(Contact.objects.first(), test_object)
+        self.assertIn('Olexandr', response.content)
